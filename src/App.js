d80599f341a2ec2a3aa2871f5aab08fb5485e684
Niranjan420 <niranjanrajesh02@gmail.com>
2020-08-09 17:28:00 +0530

    ImageSearchForm + Rank + Particles done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "./App.css";
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Rank() {
+  return (
+    <div>
+      <div className="white f3">{"User, your current rank is..."}</div>
+      <div className="white f1">{"#3"}</div>
+    </div>
+  );
+}
